796b878725dd81031b22f83cfa859aa2f8a7ed99
Larry Tooley <larry@larrytooley.com>
2015-07-19 22:56:57 -0400

    Format entertainment.py and media.py to PEP8.

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,52 +6,47 @@ import fresh_tomatoes
 
 
 iron_man = media.Movie('Iron Man',
-					'''A billionaire industrialist and genius inventor, Tony 
-					Stark (Robert Downey Jr.), is conducting weapons tests 
-					overseas, but terrorists kidnap him to force him to build a 
-					devastating weapon. Instead, he builds an armored suit and 
-					upends his captors. Returning to America, Stark refines the 
-					suit and uses it to combat crime and terrorism.''',
-					'https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG',
-					'https://www.youtube.com/watch?v=8OCJRbzdzaU')
+                       'A billionaire industrialist and genius inventor, Tony Stark (Robert Downey Jr.), is conducting weapons tests overseas, but terrorists kidnap him to force him to build a devastating weapon. Instead, he builds an armored suit and upends his captors. Returning to America, Stark refines the suit and uses it to combat crime and terrorism.',
+                       'https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG',
+                       'https://www.youtube.com/watch?v=8OCJRbzdzaU')
 
 
 back_to_the_future = media.Movie('Back to the Future',
-						'''A teenager (Michael J. Foxx) time travels to the past
-						 and gets hit on by his mom.''',
-						'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg',
-						'https://www.youtube.com/watch?v=qvsgGtivCgs')
+                                 'A teenager (Michael J. Foxx) time travels to the past and gets hit on by his mom.',
+                                 'https://upload.wikimedia.org/wikipedia/en/d/d2/Back_to_the_Future.jpg',
+                                 'https://www.youtube.com/watch?v=qvsgGtivCgs')
 
 
 tron = media.Movie('Tron',
-					'''Kevin Flynn, computer programmer, finds a way into the 
-					machine and has to battle his way out.''',
-					'https://upload.wikimedia.org/wikipedia/en/1/17/Tron_poster.jpg',
-					'https://www.youtube.com/watch?v=3efV2wqEjEY')
+                   'Kevin Flynn, computer programmer, finds a way into the machine and has to battle his way out.',
+                   'https://upload.wikimedia.org/wikipedia/en/1/17/Tron_poster.jpg',
+                   'https://www.youtube.com/watch?v=3efV2wqEjEY')
 
 
 tron_legacy = media.Movie('Tron: Legacy',
-					'''Years after Sam Flynn\'s father disappeared into the 
-					machine, Sam follows in his foot steps.''',
-					'https://upload.wikimedia.org/wikipedia/en/c/c2/Tron_Legacy_poster.jpg',
-					'https://www.youtube.com/watch?v=L9szn1QQfas')
+                          'Years after Sam Flynn\'s father disappeared into the machine, Sam follows in his foot steps.',
+                          'https://upload.wikimedia.org/wikipedia/en/c/c2/Tron_Legacy_poster.jpg',
+                          'https://www.youtube.com/watch?v=L9szn1QQfas')
 
 
 space_camp = media.Movie('Space Camp',
-					'''A group of kids get more than they could imagine when 
-					they attend space camp.''',
-					'https://upload.wikimedia.org/wikipedia/en/b/b2/Space_camp_-_1986_Poster.png',
-					'https://www.youtube.com/watch?v=0Umy8VTiKG4')
+                         'A group of kids get more than they could imagine when they attend space camp.',
+                         'https://upload.wikimedia.org/wikipedia/en/b/b2/Space_camp_-_1986_Poster.png',
+                         'https://www.youtube.com/watch?v=0Umy8VTiKG4')
 
 
 iron_eagle = media.Movie('Iron Eagle',
-					'''A high school student teams with a Air Force pilot to 
-					rescue his father.''',
-					'https://upload.wikimedia.org/wikipedia/en/0/04/Iron_eagle.jpg',
-					'https://www.youtube.com/watch?v=3npr0RKBucc')
+                         'A high school student teams with a Air Force pilot to rescue his father.',
+                         'https://upload.wikimedia.org/wikipedia/en/0/04/Iron_eagle.jpg',
+                         'https://www.youtube.com/watch?v=3npr0RKBucc')
 
 # List of movies to display on page in alphabetical order.
-movies = [back_to_the_future, iron_eagle, iron_man, space_camp, tron, tron_legacy]
+movies = [back_to_the_future,
+          iron_eagle,
+          iron_man,
+          space_camp,
+          tron,
+          tron_legacy]
 
 if __name__ == '__main__':
     fresh_tomatoes.open_movies_page(movies)
